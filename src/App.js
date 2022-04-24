cd2aa5cc3c5908dc8bedb32b9113b21f807363b4
Thilina Ekanayaka <tekanayaka@accelaero.com>
2022-04-24 13:18:09 +0530

    getting chart labels

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import { Link } from "react-router-dom";
 import { Extended } from './components/Extended';
 import { Dashboard } from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
+      {/* <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav> */}
       <Dashboard />
-      <Extended />
+      {/* <Extended /> */}
     </div>
   );
 }
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -9,6 +9,8 @@ import {
   Legend,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
+
+const axios = require('axios');
 
 ChartJS.register(
   CategoryScale,
@@ -39,25 +41,45 @@ const options = {
 };
 
 const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const dataset = [18, 5, 19, 27, 29, 19, 20];
 
 const data = {
   labels,
   datasets: [
     {
       label: 'Dataset 1',
-      data: [18, 5, 19, 27, 29, 19, 20],
+      data: dataset,
       borderColor: 'rgb(255, 99, 132)',
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      maxBarThickness: 40
     },
-    {
-      label: 'Dataset 2',
-      data: [18, 5, 19, 27, 29, 19, 20],
-      borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    },
+    // {
+    //   label: 'Dataset 2',
+    //   data: [18, 5, 19, 27, 29, 19, 20],
+    //   borderColor: 'rgb(53, 162, 235)',
+    //   backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    // },
   ],
 };
 
 export const Dashboard = () => {
+  useEffect(() => {
+    axios.get('https://api.covid19api.com/summary')
+      .then((response) => {
+        console.log("reponse", response);
+
+        const labels = [];
+        response.data.Countries.map((country) => {
+          labels.push(country.Country)
+        });
+
+        console.log("labels", labels);
+
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }, []);
+
   return <Bar options={options} data={data} />;
 }
